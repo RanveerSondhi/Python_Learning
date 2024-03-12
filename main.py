ae2cbcfc3bd47ff50c45528ab4369c0ff2ed924f
RanveerSondhi <160118236+RanveerSondhi@users.noreply.github.com>
2024-03-12 09:32:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+age = int(input("What is your age? "))
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  if age > 18:
+    print("You have to pay $12.")
+  elif age < 12:
+    print("You have to pay $5.")
+  else:
+    print("You have to pay $7.")
+else:
+  print("Sorry, you have to grow taller before you can ride.")
